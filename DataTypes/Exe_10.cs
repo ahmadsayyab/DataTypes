de02e00bb8eadcdf3d9320837f6e192fb8b973e9
Sayyab Khan <sayyabkhan007@gmail.com>
2023-09-06 15:31:05 -0700

    Solved Exercise 10

---
+++ DataTypes/Exe_10.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    internal class Exe_10
+    {
+        //10. Write a C# Sharp program that takes two numbers as input and
+        //returns true or false when both numbers are even or odd.
+
+        public static void CheckInput()
+        {
+            Console.WriteLine("Enter num1");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter num2");
+            int num2 = int.Parse(Console.ReadLine());
+
+            bool result;
+
+            if((num1 % 2 == 0 && num2 % 2 == 0) || (num1 % 2 != 0 && num2 % 2 != 0))
+            {
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
