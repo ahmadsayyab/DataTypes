f6bdf5fad6f64cee0520be0d31f55e3cd9d52f11
Sayyab Khan <sayyabkhan007@gmail.com>
2023-09-06 16:48:33 -0700

    Solved Exercise 11

---
+++ DataTypes/Exe_11.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DataTypes
+{
+    internal class Exe_11
+    {
+        //11. Write a C# Sharp program that takes a decimal number as input
+        //and displays its equivalent in binary form.
+
+
+        public static void DecimalToBinary()
+        {
+
+            
+
+            Console.WriteLine("Enter a decimal number to convert it to binary");
+            decimal decimalNumber = decimal.Parse(Console.ReadLine());
+
+            string binaryRepresentation = Convert.ToString((long)decimalNumber, 2);
+
+            Console.WriteLine($"Binary representation: {binaryRepresentation}");
+
+        }
+
+    }
+}
