de400093b7c2a2be30bd889b5789f741ee3a9d5a
Sayyab Khan <sayyabkhan007@gmail.com>
2023-09-06 13:58:56 -0700

    Solved Exercise 6

---
+++ DataTypes/Exe_6.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    internal class Exe_6
+    {
+        //6. Write a C# Sharp program to display certain values of the function x = y2 + 2y + 1
+        //(using integer numbers for y, ranging from -5 to +5).
+
+        public static void CalcXvalue()
+        {
+
+            Console.WriteLine("Formula Used: x = y2 + 2y + 1");
+
+
+            int y = -5;
+            while (y == -5 || y <= 5)
+            {
+                int x = (y * y) + 2 * y + 1;
+                Console.WriteLine($"y = {y}, x = {x}");
+                y++;
+
+
+            }
+        }
+    }
+}
