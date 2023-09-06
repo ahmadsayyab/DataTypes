b85af8a6db40e65641f1cdf50b78fff6783fd6da
Sayyab Khan <sayyabkhan007@gmail.com>
2023-09-06 12:38:38 -0700

    Solved Exercise 2

---
+++ DataTypes/Exe_2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    internal class Exe_2
+    {
+        //2. Write a C# Sharp program that takes
+        //a number and a width also a number. It then displays
+        //a triangle of that width using that number.
+
+        public static void PrintTriangle()
+        {
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your desired length");
+            int len = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= len; i++)
+            {
+                for (int j = i; j <= len; j++)
+                {
+                    Console.Write(num);
+                    
+                }
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
